cd7a514683c351564b158928fc412369b36e596a
mellem <cph-ml474@cphbusiness.dk>
2018-10-11 20:32:07 +0200

    changes

--- core/snoefter.py
+++ core/snoefter.py
@@ -1,7 +1,23 @@
 from scapy.all import sniff
+from scapy.packet import Packet
 import logging
 
-def _http_sniffer(method='GET', filter='tcp port 8081', count=30):
+
+######## Main ########
+
+def main():
+    http_sniff_logger()
+    #http_sniffer()
+
+
+
+######## Top level functions ########
+
+def http_sniffer(method='GET', filter='tcp port 80', count=0):
+    '''Top level function
+
+    '''
+
     raw = []
     sniff(count=count, filter=filter, prn=lambda x: raw.append(x))
 
@@ -10,27 +26,36 @@ def _http_sniffer(method='GET', filter='tcp port 8081', count=30):
     print(http_data)
 
 
-def http_sniff_logger(method='GET', filter='tcp port 8081', count=30):
+def http_sniff_logger(method='GET', filter='tcp port 80', count=0):
+    '''Top level function
+
+    '''
+
     logging.basicConfig(filename=f'http.{method}.log', level=logging.INFO)
 
     sniff(count=count, filter=filter, prn=lambda x: _http_packets_logger(x, method))
 
 
-def _http_packets_logger(packet, method='GET'):
 
-    if str(packet).find(method) > 0:
-        logging.log(logging.INFO, _http_parser(packet, method))
-
+######## Filter functions ########
 
 def _http_parser(packet, method='GET'):
+    ''' Parsing packet to a python dictionary
+
+    '''
+
     packet_str = str(packet)
     index_get = packet_str.find(method)
     req = {}
 
+    # IP information of source and destination
+    req['IP_src'] = packet.getlayer('IP').src
+    req['IP_dst'] = packet.getlayer('IP').dst
+
     parsing = ''.join(packet_str[index_get:]) # only get part containing http data
     headers_list = parsing.split('\\r\\n') # split between different headers
-    
-    req['Method'] = headers_list[0] # first item is always the method
+
+    req['Method'] = headers_list[0][0:4] # first item is always the method
 
     # run through rest of request data and pull out header data
     for i in range(1, len(headers_list)):
@@ -39,6 +64,17 @@ def _http_parser(packet, method='GET'):
         
     return req
 
+
+######## Logger functions ########
+
+def _http_packets_logger(packet, method='GET'):
+
+    if str(packet).find(method) > 0:
+        logging.log(logging.INFO, _http_parser(packet, method))
+
+
+
+######## To python-collection functions ########
 
 def _http_packets_aslist(raw_packets, method='GET'):
     ''' Returns a list of dictionaries, containing request data
@@ -65,9 +101,12 @@ def http_packets_generator(raw_packets, method='GET'):
     packets = (_http_parser(packet) for packet in raw_packets if str(packet).find(method) > 0)
     return packets
 
+
+######## div ########
+
 def tostring(packet):
     return(str(packet))
 
 
 if __name__ == '__main__':
-    http_sniff_logger()
+    main()
